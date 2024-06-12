21afb19e239d9047947ecc34fa11ebdfff370235
John Frommeyer <johnfr@seas.upenn.edu>
2024-06-12 16:54:04 -0400

    Updating AWS SDK. Switch from CommonJS to module. Switch from callback to async/await.

--- source/app.js
+++ source/app.js
@@ -1,31 +1,51 @@
 'use strict';
 
-var AWS = require('aws-sdk');
-var ssm = new AWS.SSM({region: 'us-east-1'});
+import {SSMClient, GetParametersCommand} from "@aws-sdk/client-ssm";
+
+const ssm = new SSMClient({
+    region: 'us-east-1'
+});
 
 // Parse the environment name from the lambda function name: us-east-1.dev-app-lambda-use1
-var environmentName = process.env.AWS_LAMBDA_FUNCTION_NAME.split(".")[1].split("-")[0];
+const environmentName = process.env.AWS_LAMBDA_FUNCTION_NAME.split(".")[1].split("-")[0];
 
 // If lambda name contains a uniq-id, get it. Eg. us-east-1.dev-edc-app-lambda-use1
 if (process.env.AWS_LAMBDA_FUNCTION_NAME.split(".")[1].split("-").length == 5) {
-  var uniqId = process.env.AWS_LAMBDA_FUNCTION_NAME.split(".")[1].split("-")[1] + "-";
+    var uniqId = process.env.AWS_LAMBDA_FUNCTION_NAME.split(".")[1].split("-")[1] + "-";
 } else {
-  var uniqId = "";
-};
+    var uniqId = "";
+}
+;
 
-var paramName = '/' + environmentName + '/' + uniqId + 'app-lambda/content-security-policy';
+const paramName = '/' + environmentName + '/' + uniqId + 'app-lambda/content-security-policy';
 
-const policies = ['script', 'style' ,'worker', 'img', 'font', 'media', 'frame', 'connect']
+// exported for testing
+export const policies = ['script', 'style', 'worker', 'img', 'font', 'media', 'frame', 'connect']
 
-var paramNames = {
+// exported for testing
+export const paramNames = {
     Names: policies.map(p => paramName + '/' + p)
 };
 
-// only exported for use in test
-exports.paramNames = paramNames
-exports.policies = policies
+const command = new GetParametersCommand(paramNames)
 
-function setHeaders(event, contentSecurityPolicy, callback) {
+// exported for testing
+export function setHeaders(response, contentSecurityPolicy) {
+    const headers = response.headers;
+
+    headers['strict-transport-security'] = [{
+        key: 'Strict-Transport-Security',
+        value: "max-age=31536000; includeSubdomains; preload"
+    }];
+    headers['x-frame-options'] = [{key: 'X-Frame-Options', value: "DENY"}];
+    headers['x-xss-protection'] = [{key: 'X-XSS-Protection', value: "1; mode=block"}];
+    headers['referrer-policy'] = [{key: 'Referrer-Policy', value: "same-origin"}];
+    headers['content-security-policy'] = [{key: 'Content-Security-Policy', value: contentSecurityPolicy}];
+
+    return response
+};
+
+export const handler = async (event, context) => {
     // if no response, just short circuit
     const records = event && event.Records && event.Records[0];
     const response = records && records.cf && records.cf.response;
@@ -33,27 +53,17 @@ function setHeaders(event, contentSecurityPolicy, callback) {
     if (!response || event && event['detail-type'] == 'Scheduled Event') {
         return;
     }
-
-    const headers = response.headers;
-
-    headers['strict-transport-security'] = [{key:'Strict-Transport-Security', value: "max-age=31536000; includeSubdomains; preload"}];
-    headers['x-frame-options']           = [{key:'X-Frame-Options', value:"DENY"}];
-    headers['x-xss-protection']          = [{key:'X-XSS-Protection', value:"1; mode=block"}];
-    headers['referrer-policy']           = [{key:'Referrer-Policy', value:"same-origin"}];
-    headers['content-security-policy']   = [{key: 'Content-Security-Policy', value: contentSecurityPolicy}];
-
-    callback(null, response);
-};
-
-exports.handler = (event, context, callback) => {
-    ssm.getParameters(paramNames, function(err, data) {
-        if (err) {
-            console.log(err, err.stack);
-        } else if (data.InvalidParameters && data.InvalidParameters.length > 0) {
-            console.log("invalid parameters", data.InvalidParameters)
+    try {
+        const response = await ssm.send(command)
+        if (response.InvalidParameters && response.InvalidParameters.length > 0) {
+            console.log("invalid parameters", response.InvalidParameters)
+            return response
         } else {
-           var contentSecurityPolicy = data.Parameters.map(p => p.Value).join(' ')
-           setHeaders(event, contentSecurityPolicy, callback);
+            const contentSecurityPolicy = response.Parameters.map(p => p.Value).join(' ')
+            return setHeaders(response, contentSecurityPolicy);
         }
-    })
+    } catch (err) {
+        console.log(err, err.stack);
+        return response
+    }
 };
--- source/test/simple.spec.js
+++ source/test/simple.spec.js
@@ -1,10 +1,19 @@
-const test = require('ava');
-const app = require('../app')
+import test from 'ava';
+import {paramNames, policies, setHeaders} from '../app.js'
 
 test('paramNames test', t => {
-    const names = app.paramNames.Names
+    const names = paramNames.Names
     t.is(names.length, 8)
-    for (const policy of app.policies) {
+    for (const policy of policies) {
         t.true(names.includes(`/test/app-lambda/content-security-policy/${policy}`))
     }
+});
+
+test('setHeaders() test', t => {
+    const response = {'headers': {}};
+    const contentSecurityPolicy = 'fake content security policy';
+    const modifiedResponse = setHeaders(response, contentSecurityPolicy);
+
+    t.is(modifiedResponse.headers['content-security-policy'][0].value, contentSecurityPolicy);
+    t.is(modifiedResponse.headers['referrer-policy'][0].value, "same-origin");
 });
